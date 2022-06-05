56246f0f0ec5d1e8fa7dbec69b797953146cdf7b
Yehor Sapon <egorsapon@gmail.com>
2022-06-05 15:10:41 +0300

    qwewqqwe

--- homework.py
+++ homework.py
@@ -1,10 +1,10 @@
 weight = int(input("Увядзіце вашу вагу ў кг: "))
 height = int(input("Увядзіце ваш рост у см: "))
-BMI = (weight//((height/100)**2))
-print(f"Ваш Індэкс масы цела (ІМЦ): {BMI}")
+BMI = ( weight//((height/100)**2) )
+print( f"Ваш Індэкс масы цела (ІМЦ): {BMI}" )
 number1 = int((BMI-10)//3)
-number2 = (20 - number1)
-print("10","="*number1,"|","="*number2,"70")
+number2 = ( 20 - number1 )
+print( "10" + "="*number1 + "|" +"="*number2 + "70" )
 """
 print(" менш за 18,5 кг/м² — нястача масы цела \n",
         "18,5—25 кг/м² — здаровая маса цела \n",
