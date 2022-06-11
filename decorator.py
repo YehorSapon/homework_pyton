4b3f6bdd03ba00ac246ee115e3febf4dda3e586f
YehorSapon <87062039+YehorSapon@users.noreply.github.com>
2022-06-11 20:14:58 +0300

    Add files via upload

---
+++ decorator.py
@@ -0,0 +1,30 @@
+"""Decorators homework."""
+
+# абрамлнне *****
+
+
+def stars(func):
+    """Stars funcion."""
+    def inner(*args, **kwargs):
+        print("**********")
+        func(*args, **kwargs)
+        print("**********")
+        return inner
+
+
+# decorator syntax
+
+
+@stars
+def hello_world():
+    """Hello_world funcion."""
+    print("hello world!")
+
+
+# The following prints:
+# **********
+# hello world!
+# **********
+
+
+hello_world()
