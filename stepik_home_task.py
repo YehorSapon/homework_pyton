a3222763d0b82329e90d6e192d992a230faaf2d7
YehorSapon <87062039+YehorSapon@users.noreply.github.com>
2022-08-29 21:07:28 +0300

    Update stepik_home_task.py

--- stepik_home_task.py
+++ stepik_home_task.py
@@ -1,28 +1,44 @@
+"""Coding DNA.
+
+Кодирование осуществляется следующим образом:
+s = 'aaaabbсaa' преобразуется в 'a4b2с1a2',
+то есть группы одинаковых символов исходной строки
+заменяются на этот символ и количество его повторений
+в этой позиции строки.
+
+Напишите программу, которая считывает строку,
+кодирует её предложенным алгоритмом и выводит
+закодированную последовательность на стандартный вывод.
+Кодирование должно учитывать регистр символов.
+"""
+
 dna = input()
 new = ""
 cnt = 1
 L = int(len(dna))
-first_chair = dna[1]
+first_chair = dna[0]
 last_chair = dna[-1]
 chair = first_chair
 new = f"{chair}{cnt}"
-for i in range(1,(L-1), 1):
-    if dna[i] == dna[i-1]:
+if L > 1:
+    for i in range(1, (L-1), 1):
+        if dna[i] == dna[i-1]:
+            cnt += 1
+            chair = dna[i-1]
+            new = new[:-1] + str(cnt)
+        else:
+            cnt = 1
+            chair = dna[i]
+            new += f"{chair}{cnt}"
+    if last_chair == dna[-2]:
         cnt += 1
-        chair = dna[i-1]
-        new = new[:-1] + str(cnt)
+        chair = dna[-2]
+        new = new[:-2] + f"{chair}{cnt}"
     else:
         cnt = 1
-        cnair = dna[i]
+        chair = last_chair
         new += f"{chair}{cnt}"
-
-if last_chair == dna[-2]:
-    cnt += 1
-    chair = dna[-2]
-    new = new[:-2] + f"{chair}{cnt}"
+    print(new)
 else:
-    cnt = 1
-    chair = last_chair
-    new += f"{chair}{cnt}"
-
-print(new)
+    print(new)
+input("Put in enter to out")
