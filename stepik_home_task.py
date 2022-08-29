dba870bda64edf338edae8bdf6c695affd0a32df
YehorSapon <87062039+YehorSapon@users.noreply.github.com>
2022-08-29 22:09:09 +0300

    Update stepik_home_task.py

--- stepik_home_task.py
+++ stepik_home_task.py
@@ -13,32 +13,38 @@ s = 'aaaabbсaa' преобразуется в 'a4b2с1a2',
 """
 
 dna = input()
-new = ""
-cnt = 1
-L = int(len(dna))
-first_chair = dna[0]
-last_chair = dna[-1]
-chair = first_chair
-new = f"{chair}{cnt}"
-if L > 1:
-    for i in range(1, (L-1), 1):
-        if dna[i] == dna[i-1]:
+if dna:
+    new = ""
+    cnt = 1
+    L = int(len(dna))
+    first_chair = dna[0]
+    last_chair = dna[-1]
+    chair = first_chair
+    new = f"{chair}{cnt}"
+    if L > 1:
+        for i in range(1, (L-1)):
+            if dna[i] == dna[i-1]:
+                cnt += 1
+                chair = dna[i-1]
+                if cnt > 9:
+                    new = new[:-2] + str(cnt)
+                else:
+                    new = new[:-1] + str(cnt)
+            else:
+                cnt = 1
+                chair = dna[i]
+                new += f"{chair}{cnt}"
+        if last_chair == dna[-2]:
             cnt += 1
-            chair = dna[i-1]
-            new = new[:-1] + str(cnt)
+            chair = dna[-2]
+            new = new[:-2] + f"{chair}{cnt}"
         else:
             cnt = 1
-            chair = dna[i]
+            chair = last_chair
             new += f"{chair}{cnt}"
-    if last_chair == dna[-2]:
-        cnt += 1
-        chair = dna[-2]
-        new = new[:-2] + f"{chair}{cnt}"
+        print(new)
     else:
-        cnt = 1
-        chair = last_chair
-        new += f"{chair}{cnt}"
-    print(new)
+        print(new)
 else:
-    print(new)
+    print(dna)
 input("Put in enter to out")
