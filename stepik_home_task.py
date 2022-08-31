7ee0673e4f619794922403d037c2475afdf834f6
YehorSapon <87062039+YehorSapon@users.noreply.github.com>
2022-08-31 10:19:37 +0300

    Update stepik_home_task.py

--- stepik_home_task.py
+++ stepik_home_task.py
@@ -10,58 +10,54 @@ s = 'aaaabbсaa' преобразуется в 'a4b2с1a2',
 Кодирование должно учитывать регистр символов.
 """
 
-def get_new(el):
-    cnt += 1
-    if cnt <= 10:
-        new = new[:-1] + str(cnt)
-    elif cnt >= 99:
-        new = new[:-3] + str(cnt)
-    else:
-        new = new[:-2] + str(cnt)
-    return new
 
 dna = input()
-if not dna:
-    print(dna)
+new = ""
+cnt = 1
+L = int(len(dna))
+first_chair = dna[0]
+last_chair = dna[-1]
+chair = first_chair
+new = f"{chair}{cnt}"
+if L == 1:
+    print(new)
+elif L == 0:
+    print()
 else:
-    new = ""
-    cnt = 1
-    L = int(len(dna))
-    first_chair = dna[0]
-    last_chair = dna[-1]
-    chair = first_chair
-    new = f"{chair}{cnt}"
-    if L == 1:
-        print(new)
-    elif L == 0:
-        print()
-    else:
-        for i in range(1, (L-1)):
-            if dna[i] == dna[i-1]:
-                chair = dna[i-1]
-                cnt += 1
-                if cnt <= 10:
-                    new = new[:-1] + str(cnt)
-                elif cnt >= 100:
-                    new = new[:-3] + str(cnt)
-                else:
-                    new = new[:-2] + str(cnt)
-            else:
-                cnt = 1
-                chair = dna[i]
-                new += f"{chair}{cnt}"
-        if last_chair == dna[-2]:
-            chair = dna[-2]
+    for i in range(1, (L-1)):
+        if dna[i] == dna[i-1]:
+            chair = dna[i-1]
             cnt += 1
             if cnt <= 10:
-                new = new[:-1] + f"{cnt}"
-            elif cnt >= 100:
-                new = new[:-3] + f"{cnt}"
+                new = new[:-1] + str(cnt)
+            elif 101 <= cnt <= 1000:
+                new = new[:-3] + str(cnt)
+            elif 1001 <= cnt <= 10000:
+                new = new[:-4] + str(cnt)
+            elif 10001 <= cnt <= 100000:
+                new = new[:-5] + str(cnt)
             else:
-                new = new[:-2] + f"{cnt}"
+                new = new[:-2] + str(cnt)
         else:
             cnt = 1
-            chair = last_chair
+            chair = dna[i]
             new += f"{chair}{cnt}"
-        print(new)
+    if last_chair == dna[-2]:
+        chair = dna[-2]
+        cnt += 1
+        if cnt <= 10:
+            new = new[:-1] + str(cnt)
+        elif 101 <= cnt <= 1000:
+            new = new[:-3] + str(cnt)
+        elif 1001 <= cnt <= 10000:
+            new = new[:-4] + str(cnt)
+        elif 10001 <= cnt <= 100000:
+            new = new[:-5] + str(cnt)
+        else:
+            new = new[:-2] + str(cnt)
+    else:
+        cnt = 1
+        chair = last_chair
+        new += f"{chair}{cnt}"
+    print(new)
 input("Put in enter to out")
